54e777a428f4bbef5214a9ffd1e7405a785fdd73
Jacob Nickerson <JacobNickerson817@gmail.com>
2024-09-10 12:37:27 -0400

    Added two new command line flags, '-W'/'--width' and '-H'/'--height' that can be used to set the window size, if only one flag is used then the other parameter is automatically set at a 16:9 ratio, if no flag is used then the default window size is 1280x720

--- src/main.cpp
+++ src/main.cpp
@@ -11,24 +11,35 @@ using namespace std::chrono_literals;
 int main(int argc, char* argv[]) {
     size_t data_point_count;
     bool data_point_set = false;
+    int window_width;
+    bool window_width_set = false;
+    int window_height;
+    bool window_height_set = false;
+    const sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
     for (int i = 1; i < argc; i++) {
         std::string arg = argv[i];
         if (arg == "-h" || arg == "--help") {
-            std::cout << "usage: VisSort [options]" << '\n';
+            std::cout << "usage: vis-sort [options]" << '\n';
             std::cout << "options: " << '\n';
-            std::cout << "-h, --help   Displays this menu" << '\n';
-            std::cout << "-c, --count <number>   Sets data point count to number" << std::endl;
+            std::cout << "-h, --help             Displays this menu" << '\n';
+            std::cout << "-c, --count <number>   Sets data point count to number" << '\n';
+            std::cout << "-H, --height           Sets the window height, width is automatically set if not specified with -W" << '\n';
+            std::cout << "-W, --width            Sets the window width, height is automatically set if not specified with -H" << '\n';
             return 0;
         } else if (arg == "-c" || arg == "--count") {
-            if (argc <= i+1) {
-                std::cerr << "Error: Missing number" << '\n';
-                std::cout << "Usage: VisSort -c <number>" << std::endl;
+            if (data_point_set) {
+                std::cerr << "Error: Data point count already set" << std::endl;
                 return 1;
             }
+            if (argc <= i+1) {
+                std::cerr << "Error: Missing number" << '\n';
+                std::cout << "Usage: vis-sort -c <number>" << std::endl;
+                return 1;
+            }
+
             try {
                 std::string data_point_count_string = argv[(i++)+1];
                 data_point_count = std::stoul(data_point_count_string);
-                data_point_set = true;
                 std::cout << "Data point count set successfully." << std::endl;
             } catch (const std::invalid_argument& e) {
                 std::cerr << "Error: Invalid Number" << std::endl;
@@ -36,6 +47,75 @@ int main(int argc, char* argv[]) {
             } catch (const std::out_of_range& e) {
                 std::cerr << "Error: Number is out of the valid range" << std::endl;
                 return 1;
+            }
+
+            if (data_point_count <= 0) { 
+                std::cerr << "Error: Data point count must be greater than 0" << std::endl;
+                return 1;
+            } else {
+                data_point_set = true;
+            }
+        } else if (arg == "-W" || arg == "--width") {
+            if (window_width_set) {
+                std::cerr << "Error: Window width already set" << std::endl;
+                return 1;
+            } 
+            if (argc <= i+1) {
+                std::cerr << "Error: Missing screen width" << '\n';
+                std::cout << "Usage: vis-sort -w <width>" << std::endl;
+                return 1;
+            }
+
+            try { // checking that the user inputted a valid integer
+                std::string screen_width_string = argv[(i++)+1];
+                window_width = std::stoi(screen_width_string); 
+            } catch (const std::invalid_argument& e) {
+                std::cerr << "Error: Invalid value" << std::endl;
+                return 1;
+            } catch (const std::out_of_range& e) {
+                std::cerr << "Error: Value is out of the valid range" << std::endl;
+                return 1;
+            }
+
+            if (window_width < 400) { // checking that the width is within valid bounds
+                std::cerr << "Error: Width too small" << std::endl;
+                return 1;
+            } else if (window_width > desktop.width) {
+                std::cerr << "Error: Width exceeds current screen size" << std::endl;
+                return 1;
+            } else {
+                window_width_set = true;
+            }
+        } else if (arg == "-H" || arg == "--height") {
+            if (window_height_set) {
+                std::cerr << "Error: Window height already set" << std::endl;
+                return 1;
+            } 
+            if (argc <= i+1) {
+                std::cerr << "Error: Missing screen height" << '\n';
+                std::cout << "Usage: vis-sort -H <height>" << std::endl;
+                return 1;
+            }
+
+            try {  // checking that the user inputted a valid integer
+                std::string screen_height_string = argv[(i++)+1];
+                window_height = std::stoi(screen_height_string); 
+            } catch (const std::invalid_argument& e) {
+                std::cerr << "Error: Invalid value" << std::endl;
+                return 1;
+            } catch (const std::out_of_range& e) {
+                std::cerr << "Error: Value is out of the valid range" << std::endl;
+                return 1;
+            }
+
+            if (window_height < 200) {  // checking that the height is within the valid bounds
+                std::cerr << "Error: Height too small" << std::endl;
+                return 1;
+            } else if (window_height > desktop.height) {
+                std::cerr << "Error: Height exceeds current screen size" << std::endl;
+                return 1;
+            } else {
+                window_height_set = true;
             }
         } else {
             std::cerr << "Invalid option" << std::endl;
@@ -45,14 +125,28 @@ int main(int argc, char* argv[]) {
     if (!data_point_set) {
         data_point_count = 256;
     }
-    const sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
-    const int window_width = 1280;
-    const int window_height = 720;
-    const int window_pos_x = (desktop.width - window_width) / 2;
-    const int window_pos_y = (desktop.height - window_height) / 2;
-     sf::RenderWindow window(sf::VideoMode(window_width, window_height), "VisSort", sf::Style::Close);
+    if (!window_height_set && !window_width_set) {
+        window_width = 1280;
+        window_height = 720;
+    } else if (!window_height_set) {
+        std::cout << "Automatically setting window height" << '\n';
+        std::cout << window_width << std::endl;
+        window_height = (window_width * 9) / 16;  // default to a 16:9 resolution
+        std::cout << window_height << std::endl;
+    } else if (!window_width_set) {
+        std::cout << "Automatically setting window width" << '\n';
+        std::cout << window_height << std::endl;
+        window_width = (window_height * 16) / 9;
+        std::cout << window_width << std::endl;
+    }
+    if (data_point_count > window_width) {
+        std::cerr << "Error: Too many data points to display on screen" << std::endl;
+        return 1;
+    }
+
+    sf::RenderWindow window(sf::VideoMode(window_width, window_height), "VisSort", sf::Style::Close);
     window.setSize(sf::Vector2u(window_width, window_height)); 
-    window.setPosition(sf::Vector2i(window_pos_x, window_pos_y));
+    window.setPosition(sf::Vector2i(0, 0));
     Graphics_Engine graphics(data_point_count, &window);
     Sorter sorter(data_point_count, &graphics);
     sorter.shuffleData();
